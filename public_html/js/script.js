8cf737a54c038abc7e7efe1cca936e5bb2f1362b
Tangui Hoarau <tangui.hoarau@hotmail.com>
2022-11-10 16:12:19 +0100

    commit nettoyage

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -61,4 +61,16 @@ function includePage(file, fonc = false) {
    xhttp.send();
 }
 
+let header = document.querySelector("#header");
+let lastScrollValue = 0;
+
+document.addEventListener('scroll',() => {
+		let top  = document.documentElement.scrollTop;
+    if(lastScrollValue < top) {
+    	header.classList.add("hidden");
+    } else {
+    	header.classList.remove("hidden");
+    }
+    lastScrollValue = top;
+});
 
