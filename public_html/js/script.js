c8be5f46c4ea212237bfe55d2cb4dfa604d21818
Tangui Hoarau <tangui.hoarau@hotmail.com>
2022-11-10 09:23:32 +0100

    first commit

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,1 +1,73 @@
+// création d'une instance de la class Navigo
+const router = new Navigo("/");
+
+// définition des routes
+router.on({
+    '/' : function() {
+       includePage('home');
+   },
+   '/dragons' : function() {
+       includePage('Page_Dragons');
+   },
+   '/cinematographie' : function() {
+       includePage('Page_Cinematographie');
+   },
+   '/cinematographie_films_series' : function() {
+       includePage('Page_Cinematographie_Films_Series');
+   },
+   '/cinematographie_voice_actors' : function() {
+       includePage('Page_Cinematographie_Voice_Actors');
+   },
+   '/cinematographie_musiques' : function() {
+       includePage('Page_Cinematographie_Musiques');
+   },
+   '/personnages' : function() {
+       includePage('Page_Personnages');
+   },
+   '/studio' : function() {
+       includePage('Page_Studio');
+   }
+});
+
+// démarre le routeur
+router.resolve();
+
+// fonction permettant de charger un fichier html dans le #main-content
+function includePage(file, fonc = false) {
+   var elmnt, xhttp;
+
+   // récupère l'élément #main-content
+   elmnt = document.getElementById("main-content");
+   // création d'une requête ajax
+   xhttp = new XMLHttpRequest();
+   // définition de la fonction de callback
+   xhttp.onreadystatechange = function () {
+       // si la requête est terminée
+       if (this.readyState === 4) {
+           // si la requête est un succès
+           if (this.status === 200) {
+               // affiche le contenu du fichier html dans #main-content
+               elmnt.innerHTML = this.responseText;
+               // si une fonction est passée en paramètre
+               if (fonc) {
+                   // exécute la fonction
+                   fonc(this.responseText);
+               }
+           }
+           // si la requête est un échec 404
+           else if (this.status === 404) {
+               elmnt.innerHTML = "Page not found.";
+           }
+           // tout autre code d'erreur
+           else {
+               elmnt.innerHTML = "Error: " + this.status;
+           }
+       }
+   };
+   // définition de l'adresse du fichier html à charger
+   xhttp.open("GET", "/pages/" + file + ".html", true);
+   // envoie la requête
+   xhttp.send();
+}
+
 
