4b36eb971ac6cebc6a917390161f904e97215349
Tangui Hoarau <tangui.hoarau@hotmail.com>
2022-11-14 23:22:58 +0100

    commit barre

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -78,3 +78,15 @@ document.addEventListener('scroll',() => {
     lastScrollValue = top;
 });
 
+//barre de scroll
+window.onload = () => {
+    window.addEventListener("scroll", () => {
+       let hauteur = document.documentElement.scrollHeight - window.innerHeight;
+       let position = window.scrollY;
+       let largeur = document.documentElement.clientWidth;
+       
+       let barre = (position / hauteur) * largeur;
+       
+       document.getElementById("barre").style.widht = barre + "px";
+    });
+};
