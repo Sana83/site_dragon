ec23f38ccf212f315cf700fd4348de2db32c8fef
Tangui Hoarau <tangui.hoarau@hotmail.com>
2022-12-16 04:06:41 +0100

    page dragons

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -119,6 +119,21 @@ router.on({
                 });
 
             });
+            
+            document.body.onload=function(){
+              nbr=5;
+              p=0;
+              container=document.getElementById("container");
+              g=document.getElementById("g");
+              d=document.getElementById("d");
+              container.style.widht=(800*nbr)+"px";
+              for(i=1;i<=nbr;i++){
+                  div=document.createElement("div");
+                  div.className="photo";
+                  div.style.backgroundImage="url('img/im"+i+".jpg')";
+                  container.appendChild(div);
+              }
+            };
         });
     },
     '/contact': function () {
