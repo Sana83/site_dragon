b75f7ea0434e5a56892fdf98be211bd796c81c78
Tangui Hoarau <tangui.hoarau@hotmail.com>
2022-12-18 05:22:10 +0100

    site

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -114,12 +114,14 @@ router.on({
 
             });
 
+            document.getElementById("defaultOpen").click();
+
         });
     },
 
     '/studio': function () {
         includePage('Page_Studio', function () {
-            
+
 //            bouton fleche haut
             const btn = document.querySelector('.btn_fleche_haut');
 
@@ -132,7 +134,7 @@ router.on({
                 });
 
             });
-            
+
 //            image caroussel
             nbr = 5;
             p = 0;
@@ -159,16 +161,16 @@ router.on({
                 container.style.transform = "translate(" + p * 800 + "px)";
                 container.style.transition = "all 0.5s ease";
             };
-            
+
 //            effet parallax
             document.addEventListener("mousemove", parallax);
-            function parallax(e){
-                this.querySelectorAll('.layer').forEach(layer =>{
+            function parallax(e) {
+                this.querySelectorAll('.layer').forEach(layer => {
                     const speed = layer.getAttribute('data-speed');
-                    
-                    const x = (window.innerWidth - e.pageX*speed);
-                    const y = (window.innerHeight - e.pageY*speed);
-                    
+
+                    const x = (window.innerWidth - e.pageX * speed);
+                    const y = (window.innerHeight - e.pageY * speed);
+
                     layer.style.transform = 'translateX(${x}px) translateY(${y}px)';
                 });
             }
@@ -188,6 +190,115 @@ router.on({
 
     '/jeu': function () {
         includePage('Page_Jeu', function () {
+            const canvas = document.getElementById('canvas');
+            const score = document.getElementById('score');
+            const days = document.getElementById('days');
+            const endScreen = document.getElementById('endScreen');
+
+            daysLeft = 60;
+            gameOverNumber = 50;
+            loopPlay = false;
+
+            function start() {
+                count = 0;
+                getFaster = 6000;
+                daysRemaining = daysLeft;
+
+                canvas.innerHTML = '';
+                score.innerHTML = count;
+                days.innerHTML = daysRemaining;
+
+                // make sure to not play loop several times
+                loopPlay ? '' : game();
+                loopPlay = true;
+
+                function game() {
+                    let randomTime = Math.round(Math.random() * getFaster);
+                    getFaster > 700 ? getFaster = (getFaster * 0.90) : '';
+
+                    setTimeout(() => {
+                        if (daysRemaining === 0) {
+                            youWin();
+                        } else if (canvas.childElementCount < gameOverNumber) {
+                            virusPop();
+                            game();
+                        } else {
+                            gameOver();
+                        }
+                    }, randomTime);
+                }
+                ;
+
+                const gameOver = () => {
+                    endScreen.innerHTML = `<div class="gameOver">Game over <br/>score : ${count} </div>`;
+                    endScreen.style.visibility = 'visible';
+                    endScreen.style.opacity = '1';
+                    loopPlay = false;
+                };
+
+                const youWin = () => {
+                    let accuracy = Math.round(count / daysLeft * 100);
+                    endScreen.innerHTML = `<div class="youWin">Well done ! You overcome this motherfucker<br/><span>Accuracy : ${accuracy} %</span></div>`;
+                    endScreen.style.visibility = 'visible';
+                    endScreen.style.opacity = '1';
+                    loopPlay = false;
+                };
+            }
+            ;
+
+// create random element
+            function virusPop() {
+                let virus = new Image();
+
+                virus.src = "https://i.ibb.co/9TYZnLB/pngwave.png";
+
+                virus.classList.add('virus');
+                virus.classList.add('virus-bis');
+                virus.style.top = Math.random() * 500 + 'px';
+                virus.style.left = Math.random() * 500 + 'px';
+
+                let x, y;
+                x = y = (Math.random() * 45) + 30;
+                virus.style.setProperty('--x', `${ x }px`);
+                virus.style.setProperty('--y', `${ y }px`);
+
+                let plusMinus = Math.random() < 0.5 ? -1 : 1;
+                let trX = Math.random() * 500 * plusMinus;
+                let trY = Math.random() * 500 * plusMinus;
+                virus.style.setProperty('--trX', `${ trX }%`);
+                virus.style.setProperty('--trY', `${ trY }%`);
+
+                canvas.appendChild(virus);
+            }
+            ;
+
+// remove element clicked
+            document.addEventListener("click", function (e) {
+                let targetElement = e.target || e.srcElement;
+
+                if (targetElement.classList.contains('virus')) {
+                    targetElement.remove();
+                    count++;
+                    score.innerHTML = count;
+                }
+                ;
+            });
+
+// countdown on click
+            canvas.addEventListener('click', () => {
+                if (daysRemaining > 0) {
+                    daysRemaining--;
+                    days.innerHTML = daysRemaining;
+                }
+            });
+
+// hide screen on click
+            endScreen.addEventListener('click', () => {
+                setTimeout(() => {
+                    start();
+                    endScreen.style.visibility = 'hidden';
+                }, 3500);
+            });
         });
     }
 });
